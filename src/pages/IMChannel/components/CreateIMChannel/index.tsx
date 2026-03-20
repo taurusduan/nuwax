751e970eed6983e379d661a564c3b1bbd14f5cc5
xiedaokun <102251868@qq.com>
2026-03-20 10:31:17 +0800

    feat(components): 为 SelectTarget 增加 tooltip 支持并添加安全提示
    - 在 SelectTargetProps 接口中新增 tooltip 属性。
    - 在 SelectTarget 组件参数中解构并传给 Form.Item 元素。
    - 在创建 IM 频道时为智能体选择框添加安全提示内容。

--- src/pages/IMChannel/components/CreateIMChannel/index.tsx
+++ src/pages/IMChannel/components/CreateIMChannel/index.tsx
@@ -206,6 +206,7 @@ const CreateIMChannel: React.FC<CreateIMChannelProps> = ({
         form={form}
         name="target"
         label="智能体"
+        tooltip="通用智能体将使用你的沙箱（电脑）环境执行任务，切勿将对应的机器人分享给他人使用。"
         hideTop={[
           AgentComponentTypeEnum.Knowledge,
           AgentComponentTypeEnum.Table,
