b30077054298f8cbe673a15d106eaf6a98169ef7
xiedaokun <102251868@qq.com>
2026-03-31 10:44:09 +0800

    feat(WorkspaceLayout): 添加返回按钮功能及样式调整
    - 在`WorkspaceLayout`组件中新增返回按钮功能，支持自定义返回事件。
    - 更新样式，增加返回按钮的图标样式，并调整标题区域的间距。
    - 在`ApiKeyLogs`页面中集成返回按钮，提供用户返回上一级页面的便捷操作。

--- src/pages/MorePage/ApiKeyLogs/index.tsx
+++ src/pages/MorePage/ApiKeyLogs/index.tsx
@@ -1,9 +1,19 @@
 import WorkspaceLayout from '@/components/WorkspaceLayout';
+import { history } from 'umi';
 import LogProTable from './LogProTable';
 
 const ApiKeyLogs: React.FC = () => {
+  const handleBack = () => {
+    history.push('/more-page/api-key');
+  };
+
   return (
-    <WorkspaceLayout title="Api调用日志" hideScroll={false}>
+    <WorkspaceLayout
+      title="Api调用日志"
+      hideScroll={false}
+      back={true}
+      onBack={handleBack}
+    >
       <LogProTable />
     </WorkspaceLayout>
   );
