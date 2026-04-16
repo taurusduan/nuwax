3c3b0a9e83751b2d055be19ae9d48d2c0c2cebde
xiedaokun <102251868@qq.com>
2026-04-16 14:03:48 +0800

    feat(XModalForm): 增强提交者配置的国际化支持 - 更新了 `XModalForm` 组件，使其包含自定义的 `submitter` 配置。 - 使用 `dict` 函数为提交和重置按钮的文本集成了国际化功能。 - 此变更旨在通过提供本地化的按钮标签来改善用户体验，使表单对不同语言的用户更具可访问性。

--- src/components/ProComponents/XModalForm/index.tsx
+++ src/components/ProComponents/XModalForm/index.tsx
@@ -1,9 +1,11 @@
+import { dict } from '@/services/i18nRuntime';
 import { ModalForm, ModalFormProps } from '@ant-design/pro-components';
 import React from 'react';
 
 function XModalForm<T = Record<string, any>>({
   children,
   requiredMark,
+  submitter,
   ...restProps
 }: ModalFormProps<T>) {
   // 默认 requiredMark 渲染函数，将必填标识放在后方
@@ -20,6 +22,13 @@ function XModalForm<T = Record<string, any>>({
   return (
     <ModalForm<T>
       requiredMark={requiredMark ?? defaultRequiredMark}
+      submitter={{
+        searchConfig: {
+          submitText: dict('PC.Common.Global.confirm'),
+          resetText: dict('PC.Common.Global.cancel'),
+        },
+        ...submitter,
+      }}
       {...restProps}
     >
       {children}
